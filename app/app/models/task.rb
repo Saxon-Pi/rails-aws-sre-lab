6591cecb58e7ec275347881316c65413a60ef95d
Saxon-Pi <ts.8849.p@gmail.com>
2026-08-25 15:04:44 +0900

    Rails: TaskにValidationを追加

--- app/app/models/task.rb
+++ app/app/models/task.rb
@@ -8,4 +8,7 @@ task.rb: Task というデータの扱い方を定義する
 class Task < ApplicationRecord
 # Rails側で tasksテーブルを操作するための Model
 # -> CreateTasksのMigrationで作った tasks テーブルを、Task Modelを通して操作する
+    # 入力が空なら保存失敗
+    validates :title, presence: true
+    validates :status, presence: true
 end
