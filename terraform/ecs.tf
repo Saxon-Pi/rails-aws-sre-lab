87d802d66c0970097bf54cd921403f9b0e724845
Saxon-Pi <ts.8849.p@gmail.com>
2026-09-08 13:15:41 +0900

    Terraform: ECS Service Auto Scalingを追加

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -124,3 +124,30 @@ resource "aws_ecs_service" "rails" {
     container_port   = 8080
   }
 }
+
+resource "aws_appautoscaling_target" "ecs" {
+  max_capacity       = 3
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.rails.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_cpu" {
+  name               = "rails-aws-sre-lab-cpu-scaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value = 50.0
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    scale_out_cooldown = 60
+    scale_in_cooldown  = 300
+  }
+}
